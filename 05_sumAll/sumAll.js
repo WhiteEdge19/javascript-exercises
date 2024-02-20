9cab0b5e883d892765e90a47985e65f505e902b5
WhiteEdge19 <whiteedge1@gmail.com>
2024-02-20 15:54:43 -0500

    solucion 5 java suma

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(min,max) {
+    let suma=0;
+    if (!Number.isInteger(min) || !Number.isInteger(max)){
+        return "ERROR";
+    }
+    if (min<0 || max<0){
+        return "ERROR";
+    }
+    if(max<min){
+        [min,max] = [max,min];
+    }
+    for (let i=min;i<=max ;i++){
+        suma += i;
+    }
+    return suma;
 };
 
 // Do not edit below this line
