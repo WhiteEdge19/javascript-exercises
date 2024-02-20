313bb07c8da8fb77a4ea8f0c36f53a98e9a30b0c
WhiteEdge19 <whiteedge1@gmail.com>
2024-02-20 15:02:24 -0500

    java 4 solucion

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...args) {
+    let salida = [];
+    for(let i=0; i<array.length ; i++){
+        if (!args.includes(array[i])){
+            salida.push(array[i]);
+        }
+    }
+    return salida;
 };
 
 // Do not edit below this line
